5aed054dba25ac70969710fc59ea6ed9469de515
Vladislav <itochanenko@gmail.com>
2019-02-17 15:40:48 +0200

    Added rotateRight, addNormalize and addElement mehods

--- 2 - Order Statistic Tree/Order Statistic Tree/rbtree.h
+++ 2 - Order Statistic Tree/Order Statistic Tree/rbtree.h
@@ -24,20 +24,21 @@ struct Node {
 	}
 
 	Node() = default;
-	Node(Color color, bool isNil, Product* key, Node* left, Node* right) {
+	Node(Color color, bool isNil, Product* key, Node* left, Node* right, Node* parent) {
 		this->color = color;
 		this->isNil = isNil;
 		this->key = key;
 		this->left = left;
 		this->right = right;
+		this->parent = parent;
 	}
 };
 
 class RBTree {
 private:
 
-	Node* head;
-	Node* nil = new Node(black, true, 0, NULL, NULL);
+	Node* nil = new Node(black, true, NULL, NULL, NULL, NULL);
+	Node* head = new Node(black, false, NULL, NULL, NULL, nil);
 
 	void rotateLeft(Node* node) {
 		Node* sub = node->right;
@@ -59,18 +60,94 @@ private:
 	}
 
 	void rotateRight(Node* node) {
+		Node* sub = node->left;
+		node->left = sub->right;
 
+		if (sub->right != nil)
+			sub->right->parent = node;
+		
+		sub->parent = node->parent;
+
+		if (node->parent == nil)
+			head = sub;
+		else if (node == node->parent->right)
+			node->parent->right = sub;
+		else node->parent->left = sub;
+
+		sub->right = node;
+		node->parent = sub;
 	}
 
-	void deleteNormalize() {
-
-	}
-
-	void addNormalize() {
-
+	void addNormalize(Node * node) {
+		Node *sub;
+		while (node->parent->color == red) {
+			if (node->parent == node->parent->parent->left) {
+				sub = node->parent->parent->right;
+				if (sub->color == red) {
+					node->parent->color = black;
+					sub->color = black;
+					node->parent->parent->color = red;
+					node = node->parent->parent;
+				}
+				else {
+					if (node == node->parent->right) {
+						node = node->parent;
+						rotateLeft(node);
+					}
+					node->parent->color = black;
+					node->parent->parent->color = red;
+					rotateRight(node->parent->parent);
+				}
+			}
+			else {
+				sub = node->parent->parent->left;
+				if (sub->color == red) {
+					node->parent->color = black;
+					sub->color = black;
+					node->parent->parent->color = red;
+					node = node->parent->parent;
+				}
+				else {
+					if (node == node->parent->left) {
+						node = node->parent;
+						rotateLeft(node);
+					}
+					node->parent->color = black;
+					node->parent->parent->color = red;
+					rotateRight(node->parent->parent);
+				}
+			}
+		}
+		head->color = black;
 	}
 
 public:
+
 	// TODO :: getElement()
-	// TODO :: addElement()
+	void addElement(Product* product) {
+		Node* node = new Node(red, false, product, NULL, NULL, NULL);
+		
+		Node *sub = nil, *point = head;
+		while (point != nil) {
+			sub = point;
+			if (node->price() < point->price())
+				point = point->left;
+			else
+				point = point->right;
+		}
+
+		node->parent = sub;
+
+		if (sub == nil)
+			head = node;
+		else if (node->price() < sub->price())
+			sub->left = node;
+		else
+			sub->right = node;
+
+		node->left = nil;
+		node->right = nil;
+
+		addNormalize(node);
+	}
 };
